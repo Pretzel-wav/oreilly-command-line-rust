bac01d9506392abb8c7d7c6a525d7b49eb6709fb
pretzel-wav <justin.macak@gmail.com>
2025-02-06 09:43:48 -0500

    changed relative paths in mk-outs.ps1

--- echor/tests/mk-outs.ps1
+++ echor/tests/mk-outs.ps1
@@ -1,4 +1,7 @@
-$out_dir = ".\expected"
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptFolder = Split-Path $scriptPath -Parent
+
+$out_dir = "$scriptFolder\expected"
 
 if (-Not (Test-Path $out_dir)) {
     New-Item -ItemType Directory -Path $out_dir
@@ -8,3 +11,5 @@ Write-Output "Hello there" > "$out_dir\hello1.txt"
 Write-Output "Hello"    "there" > "$out_dir\hello2.txt"
 Write-Output -n "Hello    there" > "$out_dir\hello1.n.txt"
 Write-Output -n "Hello" "there" > "$out_dir\hello2.n.txt"
+
+Write-Output "Created test files in $out_dir"
